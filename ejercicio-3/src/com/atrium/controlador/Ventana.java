b7f5e626cbdd845817eb64731851fa04a794a3de
Raúl <raul.hernandez@testeandosoftware.com>
2014-03-07 13:16:51 +0100

    update

--- ejercicio-3/src/com/atrium/controlador/Ventana.java
+++ ejercicio-3/src/com/atrium/controlador/Ventana.java
@@ -12,17 +12,28 @@ public class Ventana extends JFrame {
 	private JButton boton;
 
 	public Ventana() {
-		setBounds(100, 200, 800, 600);
+		setBounds(100, 200, 300, 300);
 		setResizable(false);
 		setDefaultCloseOperation(Ventana.EXIT_ON_CLOSE);
-
+		setLayout(null);
+		
 		crear_Interface();
 
 		setVisible(true);
 	}
 
 	public void crear_Interface() {
+		etiqueta=new JLabel("Envíenos su opinión");
+		texto=new JTextField();
+		boton=new JButton("Enviar");
 
+		etiqueta.setBounds(15, 30, 160, 18);
+		texto.setBounds(15, 60, 200, 18);
+		boton.setBounds(15, 90, 70, 20);
+		
+		this.add(boton);
+		this.add(texto);
+		this.getContentPane().add(etiqueta);
 	}
 
 }
