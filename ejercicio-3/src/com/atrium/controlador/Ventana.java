8a2500e706e7aa837a98f7d1844b107d618645b4
Raúl <raul.hernandez@testeandosoftware.com>
2014-03-07 13:24:45 +0100

    updated

--- ejercicio-3/src/com/atrium/controlador/Ventana.java
+++ ejercicio-3/src/com/atrium/controlador/Ventana.java
@@ -4,18 +4,30 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JProgressBar;
+import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
+import javax.swing.JSpinner;
+import javax.swing.JCheckBox;
+import javax.swing.JTextArea;
 
 public class Ventana extends JFrame {
 
 	private JLabel etiqueta;
 	private JTextField texto;
 	private JButton boton;
+	private JPasswordField passwordField;
+	private JCheckBox chckbxPrueba;
+	private JTextArea textArea;
 
 	public Ventana() {
+		setTitle("Ejercicio-3");
 		setBounds(100, 200, 300, 300);
 		setResizable(false);
 		setDefaultCloseOperation(Ventana.EXIT_ON_CLOSE);
-		setLayout(null);
+		getContentPane().setLayout(null);
 		
 		crear_Interface();
 
@@ -23,17 +35,37 @@ public class Ventana extends JFrame {
 	}
 
 	public void crear_Interface() {
-		etiqueta=new JLabel("Envíenos su opinión");
+		etiqueta=new JLabel("Inserte Usuario y Password");
+		etiqueta.setToolTipText("Inserte Usuario y Password");
+		etiqueta.setFont(new Font("Verdana", Font.PLAIN, 11));
 		texto=new JTextField();
 		boton=new JButton("Enviar");
+		boton.setDisplayedMnemonicIndex(0);
+		boton.setFont(new Font("Verdana", Font.BOLD, 11));
+		boton.setToolTipText("Enviar");
+		boton.setForeground(Color.BLACK);
+		boton.setBackground(Color.WHITE);
 
-		etiqueta.setBounds(15, 30, 160, 18);
+		etiqueta.setBounds(15, 30, 200, 18);
 		texto.setBounds(15, 60, 200, 18);
-		boton.setBounds(15, 90, 70, 20);
+		boton.setBounds(15, 120, 80, 20);
 		
-		this.add(boton);
-		this.add(texto);
+		getContentPane().add(boton);
+		getContentPane().add(texto);
 		this.getContentPane().add(etiqueta);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(15, 90, 100, 20);
+		getContentPane().add(passwordField);
+		
+		chckbxPrueba = new JCheckBox("Acepto");
+		chckbxPrueba.setSelected(true);
+		chckbxPrueba.setToolTipText("Acepto");
+		chckbxPrueba.setBounds(15, 150, 97, 23);
+		getContentPane().add(chckbxPrueba);
+		
+		textArea = new JTextArea();
+		textArea.setBounds(15, 180, 244, 56);
+		getContentPane().add(textArea);
 	}
-
 }
